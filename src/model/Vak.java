0fb78f363f17b819578f58bfc5aab4fb07013944
markamerkamp <markamerkamp@gmail.com>
2020-09-26 20:47:05 +0200

    Opdracht 3.3 BSA Monitor 2 - Onderdeel a en b

--- src/model/Vak.java
+++ src/model/Vak.java
@@ -5,4 +5,25 @@ package model;
  * Veel plezier ermee!
  */
 public class Vak {
+    private String naam;
+    private int punten;
+    private int cesuur;
+
+    public Vak(String naam, int punten, int cesuur) {
+        this.naam = naam;
+        this.punten = punten;
+        this.cesuur = cesuur;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public int getPunten() {
+        return punten;
+    }
+
+    public int getCesuur() {
+        return cesuur;
+    }
 }
