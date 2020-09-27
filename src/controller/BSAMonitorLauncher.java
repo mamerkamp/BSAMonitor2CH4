8c5d5ba55d7ec31d9d58f0d6308527042da8144a
Mark <Mark@MacBook-Pro-van-Mark.local>
2020-09-27 20:32:34 +0200

    Opdracht 2.2 BSA Monitor2 compleet

--- src/controller/BSAMonitorLauncher.java
+++ src/controller/BSAMonitorLauncher.java
@@ -1,12 +1,18 @@
 package controller;
 
+import model.Score;
 import model.Vak;
+
+import java.util.Scanner;
 
 /**
  * Author: Mark Amerkamp (markamerkamp@gmail.com)
  * Veel plezier ermee!
  */
 public class BSAMonitorLauncher {
+    static final double HOOGSTEINVOER = 10.0;
+    static final double LAAGSTEINVOER = 1.0;
+    static final double STUDIEADVIESDELER = 0.833333333;
 
     public static void main(String[] args) {
 
@@ -17,7 +23,40 @@ public class BSAMonitorLauncher {
         vakkenlijst[3] = new Vak("Personal Skills", 2, 5.5);
         vakkenlijst[4] = new Vak("Project skills", 2, 5.5);
 
+        Score[] scores = new Score[5];
 
+        Scanner keyboard = new Scanner(System.in);
+        int studiepunten = 0;
 
+        System.out.println("Voer behaalde cijfers in: ");
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print(vakkenlijst[i].getNaam() + ": ");
+            double cijfer = keyboard.nextDouble();
+            if (cijfer < LAAGSTEINVOER || cijfer > HOOGSTEINVOER){
+                System.out.println("Voer een geldige waarde in, ei!");
+                System.out.print(vakkenlijst[i].getNaam() + ": ");
+                cijfer = keyboard.nextDouble();
+            }
+            scores[i] = new Score(vakkenlijst[i], cijfer);
+            studiepunten = studiepunten + scores[i].getBehaaldePunten();
+        }
+
+        int totaalTeBehalenPunten = 0;
+        for (int i = 0; i < vakkenlijst.length; i++) {
+            totaalTeBehalenPunten = totaalTeBehalenPunten + vakkenlijst[i].getPunten();
+        }
+
+        for (int i = 0; i < vakkenlijst.length; i++) {
+            System.out.printf("Vak/project: %-40s Cijfer: %3.1f Behaalde punten: %d\n",
+                    vakkenlijst[i].getNaam(), scores[i].getCijfer(), scores[i].getBehaaldePunten());
+        }
+
+        System.out.println();
+        System.out.println("Totaal aantal behaalde studiepunten is: " + studiepunten + "/"
+                + totaalTeBehalenPunten);
+
+        if (studiepunten / totaalTeBehalenPunten <= STUDIEADVIESDELER) {
+            System.out.println("PAS OP: je ligt op schema voor een negatief BSA!");
+        }
     }
 }
